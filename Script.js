363afe57c43e6a0eaf5549c3b96ce5a5b1e0658d
AgustinLucero11 <aguslucerolp@gmail.com>
2018-09-10 08:12:58 +0200

    jquery casi andando

--- Script.js
+++ Script.js
@@ -1,3 +1,10 @@
+  
+ $(document).ready(function(){
+
+  $("#mensaje").hide();
+ });
+       
+
 $(document).ready(function(){
                 $("#enviar").click(function(){
         
@@ -24,21 +31,141 @@ $(document).ready(function(){
                     return false;
                 }
 				else{
-                $("input[type=checkbox]:checked").each(function() { 
-//cada elemento seleccionado
-alert($(this).val());
-});
-               					  
+			    var sex = $('input[name="sexo"]:checked').val()
+				
+					
+                 if( sex == null ){
+                       alert('debe completar el campo Sexo');
+					   return false ;
+                      }
+                  else 
+				  {  $("#mensaje").show(1);
+                      setTimeout(function() {
+				  $("#mensaje").hide();},5000);}					  
+                					  
 				}
+
                      
                     }
                 }
  
-            });//click
-        });//ready 
+            });
+        });
+		
+		
+	/*	$(document).ready(function(){
+			check = $('input[name="Valoracion"]:checked').val();
+					
+                 if( valo == null ){
+                       alert('debe completar el campo Sexo');
+					   return false ;
+                      }	 */
  
  
- /*window.onload = function() {
+ 
+
+
+
+var map;
+ 
+ function initialize() {
+   var punto = new google.maps.LatLng( -34.906377299999996, -57.925213899999996); //ubicación Flor de jardin
+   var myOptions = {
+     zoom: 18, 
+     center: punto,
+   }
+     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+ }
+  
+ 
+  
+ function pedirPosicion(pos) {
+   var centro = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
+   map.setCenter(centro); 
+   map.setMapTypeId(google.maps.MapTypeId.ROADMAP); 
+ 
+}
+ 
+function geolocalizame(){
+navigator.geolocation.getCurrentPosition(pedirPosicion);
+ }
+ 
+ function Negocio(){
+var punto = new google.maps.LatLng( -34.906377299999996, -57.925213899999996);
+  map.setCenter(punto);
+ }
+
+ 
+ 
+ 
+
+ 
+ 
+/*  $(document).ready(function(){
+	   var total = 0
+                    $("img.articulo").click(function(){ 
+					
+				if ($("#menu").attr("opacity")!=0.5){
+					$(this).css("opacity", 0.5);
+				    
+				} 
+				else { 
+				   $(this).css("opacity", 1);
+					}
+					$('#total').html(" Precio por articulo: $50 <br> <br> Total:$" )
+					})}); 
+					})});
+					
+					
+					
+					
+					function validarForm()
+{
+  var Nom = document.getElementById("Nom").value.trim();
+  var Ape = document.getElementById("Ape").value.trim();
+  var Fecha = document.getElementById("Fdn").value;
+  var Val = "ninguno"
+  var porNombre=document.getElementsByName("Valoracion");
+         // Recorremos todos los valores del radio button para encontrar el
+         // seleccionado
+         for(var i=0;i<porNombre.length;i++)
+         {
+             if(porNombre[i].checked)
+                 Val=porNombre[i].value;
+         }
+
+     var sexo="ninguno";
+     var porsexo=document.getElementsByName("sexo");
+         // Recorremos todos los valores del radio button para encontrar el
+         // seleccionado
+         for(var i=0;i<porsexo.length;i++)
+         {
+             if(porsexo[i].checked)
+                 sexo=porsexo[i].value;
+         }
+
+      if ( Nom.length == 0 ) {
+             alert('El campo: Nombre,  debe tener contener un valor');
+     return false;
+       }  else if ( Ape.length == 0 ) {
+         alert('El campo: Apellido, debe tener contener un valor');
+         return false;
+       } else if ( Fecha == null || Fecha==0 ) {
+         alert('El campo: Fecha, debe tener contener un valor');
+         return false;
+   }  else if ( sexo == "ninguno" ) {
+     alert('El campo: Sexo, debe tener contener un valor');
+     return false;
+   } else if ( Val == "ninguno" ) {
+     alert('El campo: Valoracion, debe tener contener un valor');
+     return false;
+   } alert('Nombre: ' + Nom + '\n' + 'Apellido: ' + Ape + '\n' +  'Nacimiento: ' + Fecha + '\n' +  'Sexo: ' + sexo + '\n' +  'Valoracion: ' + Val);
+      
+
+ */
+ 
+ 
+ window.onload = function() {
 	window.parent.document.getElementById('total').innerHTML =" Precio por articulo: $50 <br><br>Total:$0" ; }
 
 var cacheProductos=[];
@@ -84,100 +211,3 @@ function Cancel(){
       window.location = 'principal.html'
     }
 }
-
-function validarForm()
-{
-  var Nom = document.getElementById("Nom").value.trim();
-  var Ape = document.getElementById("Ape").value.trim();
-  var Fecha = document.getElementById("Fdn").value;
-  var Val = "ninguno"
-  var porNombre=document.getElementsByName("Valoracion");
-         // Recorremos todos los valores del radio button para encontrar el
-         // seleccionado
-         for(var i=0;i<porNombre.length;i++)
-         {
-             if(porNombre[i].checked)
-                 Val=porNombre[i].value;
-         }
-
-     var sexo="ninguno";
-     var porsexo=document.getElementsByName("sexo");
-         // Recorremos todos los valores del radio button para encontrar el
-         // seleccionado
-         for(var i=0;i<porsexo.length;i++)
-         {
-             if(porsexo[i].checked)
-                 sexo=porsexo[i].value;
-         }
-
-      if ( Nom.length == 0 ) {
-             alert('El campo: Nombre,  debe tener contener un valor');
-     return false;
-       }  else if ( Ape.length == 0 ) {
-         alert('El campo: Apellido, debe tener contener un valor');
-         return false;
-       } else if ( Fecha == null || Fecha==0 ) {
-         alert('El campo: Fecha, debe tener contener un valor');
-         return false;
-   }  else if ( sexo == "ninguno" ) {
-     alert('El campo: Sexo, debe tener contener un valor');
-     return false;
-   } else if ( Val == "ninguno" ) {
-     alert('El campo: Valoracion, debe tener contener un valor');
-     return false;
-   } alert('Nombre: ' + Nom + '\n' + 'Apellido: ' + Ape + '\n' +  'Nacimiento: ' + Fecha + '\n' +  'Sexo: ' + sexo + '\n' +  'Valoracion: ' + Val);
-      $(document).ready(function(){
-
-  $("#mensaje").hide();
- });
- 
- $(document).ready(function(){
-	 $("#enviar").click(function(){
- $("#mensaje").show(1);
-   setTimeout(function() {
- $("#mensaje").hide();},5000);})})
-}
-
-
-var map;
- 
- function initialize() {
-   var punto = new google.maps.LatLng( -34.906377299999996, -57.925213899999996); //ubicación Flor de jardin
-   var myOptions = {
-     zoom: 18, 
-     center: punto,
-   }
-     map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
- }
-  
- 
-  
- function pedirPosicion(pos) {
-   var centro = new google.maps.LatLng(pos.coords.latitude,pos.coords.longitude);
-   map.setCenter(centro); 
-   map.setMapTypeId(google.maps.MapTypeId.ROADMAP); 
-  alert("¡Hola! Estas en : "+pos.coords.latitude+ ","+pos.coords.longitude+" Rango de localización de +/- "+pos.coords.accuracy+" metros");
-}
- 
-function geolocalizame(){
-navigator.geolocation.getCurrentPosition(pedirPosicion);
- }
- 
-  $(document).ready(function(){
-	 $("#res").click(function(){
- $("#mensaje").hiden(); })})		 
-
- 
- 
- 
- $(document).ready(function(){
-
-  $("#mensaje").hide();
- });
- 
- $(document).ready(function(){
-	 $("#enviar").click(function(){
- $("#mensaje").show(1);
-   setTimeout(function() {
- $("#mensaje").hide();},5000);})}) */
- 
